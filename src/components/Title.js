532bf0ad403310bb2822faf46ff85f28465e502f
rick-love <ricklove@outlook.com>
2021-08-03 21:07:19 +0200

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div className="title">
+            <h1>Photo-Gram</h1>
+            <h2>My Pictures</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, quisquam.</p>
+        </div>
+    )
+}
+
+export default Title
